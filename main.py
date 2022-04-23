16d1fa5c8c8064829279c107977cf35ef502ecff
Arpudha Naresh <59797552+arpudhanaresh@users.noreply.github.com>
2022-04-23 11:12:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,12 @@ count=0;
 def autodownload():
      global count;
      while 1:
-            wget.download('http://fileupload.arpudhacloud.tk/download.php?id=1&token=fcoZgcU1hmtb8FfLAxYBRVT3tE1Hctqc&download')
+            filename = wget.download('http://fileupload.arpudhacloud.tk/download.php?id=1&token=fcoZgcU1hmtb8FfLAxYBRVT3tE1Hctqc&download')
             print("sleeping for 2 sec")
+            
             time.sleep(2)
-            print("deleting a file")
-            os.remove("100MB.bin")
+            os.remove(filename)
             print("file deleted")
-            
-            
             count = count+1;
             print("Total number of Count"+str(count));
             
